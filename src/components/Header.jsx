eaff448f5ba4476b91878722cdae8afbf374bad4
Jhens Imanel Tenorio Rojas <dataone_1999@hotmail.com>
2023-11-17 20:38:02 -0500

    segundo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 function Header() {
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            <h1>Seguimiento Pacientes {""}
-                <span className="text-indigo-600">Veterinaria</span>
-            </h1>
+            Seguimiento Pacientes {""} 
+            <span className="text-indigo-700">Veterinaria</span>
         </h1>
     )
 }
